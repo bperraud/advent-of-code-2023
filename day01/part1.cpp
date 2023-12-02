0c646d21110a415d35b186f9526ea766b67ab148
bperraud <74benji@live.fr>
2023-12-02 17:35:49 +0100

    part1 day01

--- day01/part1.cpp
+++ day01/part1.cpp
@@ -9,31 +9,23 @@ private :
 public :
 
 	int solution(const std::string &input) {
-
 		std::string line;
 		std::stringstream stream(input);
-
 		int sum = 0;
 
 		while (std::getline(stream, line)) {
-
 			std::string number;
-
 			std::for_each(line.cbegin(), line.cend(), [&](char c) {
 				if (std::isdigit(c)) {
-					if (number.size() == 0 || number.size() == 1) {
+					if (number.size() == 0 || number.size() == 1)
 						number.push_back(c);
-					}
 					else if (number.size() == 2)
 						number[1] = c;
 				} }
 			);
-
 			if (number.size() == 1)
 				number.push_back(number[0]);
-
 			sum += std::stoi(number);
-
 		}
 		return sum;
 	}
