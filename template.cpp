c23079caa2f0ad7ef7faf24e633a538aa9d756e7
bperraud <74benji@live.fr>
2023-12-02 16:43:28 +0100

    template

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <string>       // std::string
+#include <iostream>     // std::cout
+#include <sstream>      // std::stringstream
+
+class Solution {
+private :
+
+
+public :
+
+	int solution(std::string &input) {
+
+		std::string line;
+		std::stringstream stream;
+
+		while (std::getline(stream, line)) {
+
+			;
+		}
+		return 0;
+	}
+};
+
+int main () {
+
+
+
+}
