1a99ed8f743627eb69288d82737a098bcbede742
bperraud <74benji@live.fr>
2023-12-06 20:01:21 +0100

    no set

--- day04/part1.py
+++ day04/part1.py
@@ -3,8 +3,8 @@ import sys
 data = sys.stdin.read().splitlines()
 total = 0
 for line in data :
-	set1, set2 = (set(part.split()) for part in line.split(":")[1].split("|"))
-	s = 1 << (sum(elem in set2 for elem in set1))
+	part1, part2 = ((part.split()) for part in line.split(":")[1].split("|"))
+	s = 1 << (sum(elem in part1 for elem in part2))
 	total += 0 if s == 1 else s // 2
 
 print(total)
--- day04/part2.py
+++ day04/part2.py
@@ -3,8 +3,8 @@ import sys
 data = sys.stdin.read().splitlines()
 winning_card = []
 for line in data :
-	set1, set2 = (set(part.split()) for part in line.split(":")[1].split("|"))
-	winning_card.append(sum(elem in set2 for elem in set1))
+	list1, list2 = ((part.split()) for part in line.split(":")[1].split("|"))
+	winning_card.append(sum(elem in list2 for elem in list1))
 
 for card in range (len(winning_card) - 1, -1, -1) :
 	winning_card[card] = sum(winning_card[i] for i in range(card, card + winning_card[card] + 1) if i < len(winning_card))
