2c324468eacfe91ba42fb5c7a0d33f413bc61995
bperraud <74benji@live.fr>
2023-12-07 01:03:16 +0100

    refactoring

--- day05/part1.py
+++ day05/part1.py
@@ -1,20 +1,15 @@
 import sys
 
 data = sys.stdin.read().splitlines()
-
 seeds = [int(item) for item in data[0].split(":")[1].split()]
-changed_seed = [False] * len(seeds)
-del data[0]
 
-for line in data :
-	if "map" in line :
-		changed_seed = [False] * len(seeds)
+for line in data[1:]:
+	if "map" in line : changed_seed = [False] * len(seeds)
 	if len(line) > 0 and line[0].isdigit() :
-		line_split = [int(item) for item in line.split()]
+		dest, start, step = [int(item) for item in line.split()]
 		for i in range(len(seeds)):
-			if seeds[i] < line_split[1] + line_split[2] and seeds[i] >= line_split[1] and not changed_seed[i]:
-				seeds[i] += line_split[0] - line_split[1]
+			if seeds[i] < start + step and seeds[i] >= start and not changed_seed[i]:
+				seeds[i] += dest - start
 				changed_seed[i] = True
-
 
 print(min(seeds))
