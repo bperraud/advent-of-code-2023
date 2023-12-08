8b972e218cc37cfe8d3f20825178344711875ab7
bperraud <74benji@live.fr>
2023-12-08 14:16:52 +0100

    refactoring

--- day06/part1.py
+++ day06/part1.py
@@ -1,19 +1,11 @@
-
 with open('input.txt', encoding = 'utf-8') as f:
     data = [x.strip('\n') for x in f.readlines()]
 
 time_l = [int(item) for item in data[0].split(':')[1].split()]
 distance_l = [int(item) for item in data[1].split(':')[1].split()]
-res = 0
-for i, time in enumerate(time_l) :
-    record = 0
-    for j in range (1, time) :  # waiting i secondes
-        dist = (time - j) * j
-        if dist > distance_l[i] :
-            record += 1
-    if res == 0:
-        res += record
-    else :
-        res *= record
+
+res = 1
+for time, distance in zip(time_l, distance_l):
+    res *= sum((time - j) * j > distance for j in range(1, time))
 
 print(res)
