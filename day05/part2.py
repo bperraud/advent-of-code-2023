480f35b41302b5c9d563b5583fee91c6f2e977ed
bperraud <74benji@live.fr>
2023-12-08 02:17:10 +0100

    refactoring part2

--- day05/part2.py
+++ day05/part2.py
@@ -2,19 +2,17 @@ import sys
 
 data = sys.stdin.read().splitlines()
 seeds = [int(item) for item in data[0].split(":")[1].split()]
-
 seeds_start = [seeds[i] for i in range (len(seeds)) if i%2 == 0]
-seeds_range = [seeds[i] for i in range (len(seeds)) if i%2 == 1]
-seeds_dest = [seeds_start[i] + seeds_range[i] - 1 for i in range (len(seeds_start))]
+seeds_dest = [seeds[i] + seeds[i + 1] for i in range (len(seeds_start))]
 
 for line in data[1:]:
 	if "map" in line : changed_seed = []
 	if len(line) > 0 and line[0].isdigit() :
 		dest, start, step = [int(item) for item in line.split()]
 		elem_to_del = []
+
 		for i in range(len(seeds_start)):
 			destination = start + step - 1
-
 			if not (destination < seeds_start[i] or start > seeds_dest[i]) and seeds_start[i] not in changed_seed:
 				seeds_start.append(max(start, seeds_start[i]) + dest - start)
 				seeds_dest.append(min(destination, seeds_dest[i]) + dest - start)
@@ -27,10 +25,8 @@ for line in data[1:]:
 					seeds_dest.append(seeds_dest[i])
 				elem_to_del.append(i)
 
-		i = 0
-		for elem in elem_to_del:
+		for i, elem in enumerate(elem_to_del):
 			del seeds_dest[elem - i]
 			del seeds_start[elem - i]
-			i += 1
 
 print(min(seeds_start))
